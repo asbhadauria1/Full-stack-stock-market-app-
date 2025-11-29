91b55480fc235cf842708ca0f49f651b94962834
asbhadauria1 <abhadauria2006@gmail.com>
2025-11-29 18:38:53 +0800

    updated

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -34,29 +34,34 @@ module.exports.Login = async (req, res, next) => {
     if (!email || !password) {
       return res.json({ message: "All fields are required" });
     }
+
     const user = await User.findOne({ email });
     if (!user) {
       return res.json({ message: "Incorrect password or email" });
     }
+
     const auth = await bcrypt.compare(password, user.password);
     if (!auth) {
       return res.json({ message: "Incorrect password or email" });
     }
 
-    const check = await User.findOne({ email: email });
-    if (check) {
-      res.json("exist");
-    } else {
-      res.json("notexist");
-    }
+    // const check = await User.findOne({ email: email });
+    // if (check) {
+    //   res.json("exist");
+    // } else {
+    //   res.json("notexist");
+    // } 
+
     const token = createSecretToken(user._id);
     res.cookie("token", token, {
       withCredentials: true,
       httpOnly: false,
     });
+
     res
       .status(201)
       .json({ message: "User logged in successfully", success: true });
+
     next();
   } catch (error) {
     console.error(error);
